7c84d64f4b68af0e457cdb70949d07fb13169b86
taichi <ryushi@gmail.com>
2020-01-29 22:57:15 +0900

    disconnect from Server

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,17 @@ import { default as Redis } from "ioredis";
 const redis = new Redis();
 
 async function main() {
-  console.log("aaaa");
-  await redis.set("foo", "bar", "EX", 300);
-  const bar = await redis.get("foo");
-  console.log(bar);
-  await redis.del("foo");
-  const b2 = await redis.get("foo");
-  console.log(b2);
+  try {
+    console.log("aaaa");
+    await redis.set("foo", "bar", "EX", 300);
+    const bar = await redis.get("foo");
+    console.log(bar);
+    await redis.del("foo");
+    const b2 = await redis.get("foo");
+    console.log(b2);
+  } finally {
+    redis.disconnect();
+  }
 }
 
 Promise.all([main()]).catch((err: Error) => {
