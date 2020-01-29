c4f7ed0835419521d5aaced5df894d1e55f11196
taichi <ryushi@gmail.com>
2020-01-29 22:48:05 +0900

    z

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
-import config from "./config";
+import { default as Redis } from "ioredis";
+const redis = new Redis();
 
-const retry = config.get("retry");
-// tslint:disable-next-line: no-console
-console.log(retry.limit, retry.wait);
+async function main() {
+  console.log("aaaa");
+  await redis.set("foo", "bar", "EX", 300);
+  const bar = await redis.get("foo");
+  console.log(bar);
+  await redis.del("foo");
+  const b2 = await redis.get("foo");
+  console.log(b2);
+}
+
+Promise.all([main()]).catch((err: Error) => {
+  // tslint:disable-next-line: no-console
+  console.error(err);
+}).finally(() => {
+  console.log("END");
+});
